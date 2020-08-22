e5e5801985e04da59a36e1966564dd810ef6ddcb
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-22 11:47:30 +0200

    Add event listener to tabs

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -13,16 +13,38 @@ export class SideDrawer {
     this.open = false;
   }
 
+  onContentChangeHandler(content: string) {
+    console.log(content);
+  }
+
   render() {
+    let mainContent = <slot />;
+    mainContent = (
+      <div id="contact-information">
+        <h2>Content Information</h2>
+        <p>You can reach us via phone or email</p>
+        <ul>
+          <li>Phone: 245743637</li>
+          <li>
+            Email: <a href="mailto: something@somthing.com">something@something.com</a>
+          </li>
+        </ul>
+      </div>
+    );
+
     return (
       <aside>
         <header>
           <h1>{this.title}</h1>
           <button onClick={this.onCloseDrawer.bind(this)}>X</button>
         </header>
-        <main>
-          <slot />
-        </main>
+        <section id="tabs">
+          <button class="active" onClick={this.onContentChangeHandler.bind(this, 'nav')}>
+            Navigation
+          </button>
+          <button onClick={this.onContentChangeHandler.bind(this, 'content')}>Content</button>
+        </section>
+        <main>{mainContent}</main>
       </aside>
     );
   }
